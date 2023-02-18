ec64ec2aa859436e43fe415b167e8764f425fd17
macavall <43223084+macavall@users.noreply.github.com>
2023-02-17 19:15:05 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,58 @@
+﻿namespace CSharpVariables
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Good Document https://www.techrepublic.com/article/learn-the-basics-about-c-data-types-variables/#:~:text=variable%20is%200%3B%20the%20largest%20possible%20value%20is%2018%2C446%2C744%2C073%2C709%2C551%2C615.,Holds%2016%2Dbit%20Unicode%20characters.
+            
+            // Declare a variable of type int
+            int myInt;
+
+            // Assign a value to the variable
+            myInt = 5;
+
+            // Declare and assign a variable of type int
+            int myOtherInt = 10;
+
+            // Declare a variable of type string
+            string myString = "Hello World";
+
+            // Declare a variable of type bool
+            bool myBool = true;
+
+            // Declare a variable of type double
+            double myDouble = 5.5;
+
+            // Declare a variable of type float
+            float myFloat = 5.5f;
+
+            // Declare a variable of type char
+            char myChar = 'a';
+
+            // Declare a variable of type decimal
+            decimal myDecimal = 5.5m;
+
+            // Declare a variable of type long
+            long myLong = 2302140030000000L;
+
+            // Declare a variable of type short
+            short myShort = 5;
+
+            // Declare a variable of type byte
+            byte myByte = 5;
+
+            // Declare a variable of type uint
+            uint myUInt = 5;
+
+            // Declare a variable of type ulong
+            ulong myULong = 5;
+
+            // Declare a variable of type ushort
+            ushort myUShort = 5;
+
+            // Declare a variable of type sbyte
+            sbyte mySByte = 5;
+        }
+    }
+}
