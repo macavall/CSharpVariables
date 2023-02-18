79073320e6be557840f047caa38ebd22fdec73be
macavall <43223084+macavall@users.noreply.github.com>
2023-02-17 19:39:31 -0600

    update

--- Program.cs
+++ Program.cs
@@ -7,8 +7,6 @@
             // Good Document https://www.techrepublic.com/article/learn-the-basics-about-c-data-types-variables/#:~:text=variable%20is%200%3B%20the%20largest%20possible%20value%20is%2018%2C446%2C744%2C073%2C709%2C551%2C615.,Holds%2016%2Dbit%20Unicode%20characters.
             // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/language-specification/variables
 
-            
-
             // Declare a variable of type int
             int myInt;
 
@@ -73,6 +71,42 @@
             myCharArray[5] = '!';
 
             Console.ReadLine();
+
+            // To know how many bits are in a variable we
+            // can use the sizeof() Method
+
+            // 8 bits in a byte
+            // and we are going to get the number
+            // of bytes from each variable
+
+            Console.WriteLine("int: " + sizeof(int) + " bytes");
+            Console.WriteLine("bool: " + sizeof(bool) + " bytes");
+            Console.WriteLine("double: " + sizeof(double) + " bytes");
+            Console.WriteLine("float: " + sizeof(float) + " bytes");
+            Console.WriteLine("char: " + sizeof(char) + " bytes");
+            Console.WriteLine("decimal: " + sizeof(decimal) + " bytes");
+            Console.WriteLine("long: " + sizeof(long) + " bytes");
+            Console.WriteLine("short: " + sizeof(short) + " bytes");
+            Console.WriteLine("byte: " + sizeof(byte) + " bytes");
+            Console.WriteLine("uint: " + sizeof(uint) + " bytes");
+            Console.WriteLine("ulong: " + sizeof(ulong) + " bytes");
+            Console.WriteLine("ushort: " + sizeof(ushort) + " bytes");
+            Console.WriteLine("sbyte: " + sizeof(sbyte) + " bytes");
+
+            Console.WriteLine("int: " + sizeof(int) * 8 + " bits");
+            Console.WriteLine("bool: " + sizeof(bool) * 8 + " bits");
+            Console.WriteLine("double: " + sizeof(double) * 8 + " bits");
+            Console.WriteLine("float: " + sizeof(float) * 8 + " bits");
+            Console.WriteLine("char: " + sizeof(char) * 8 + " bits");
+            Console.WriteLine("decimal: " + sizeof(decimal) * 8 + " bits");
+            Console.WriteLine("long: " + sizeof(long) * 8 + " bits");
+            Console.WriteLine("short: " + sizeof(short) * 8 + " bits");
+            Console.WriteLine("byte: " + sizeof(byte) * 8 + " bits");
+            Console.WriteLine("uint: " + sizeof(uint) * 8 + " bits");
+            Console.WriteLine("ulong: " + sizeof(ulong) * 8 + " bits");
+            Console.WriteLine("ushort: " + sizeof(ushort) * 8 + " bits");
+            Console.WriteLine("sbyte: " + sizeof(sbyte) * 8 + " bits");
+
         }
     }
 }
